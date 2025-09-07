337ef6a0179bb8b747bedc11676aadd0f38a8b9b
vaughan-dsouza <vaughan.dsouza@kilowott.com>
2025-09-07 19:23:48 +0530

    added add-at

--- linked_list.cpp
+++ linked_list.cpp
@@ -32,6 +32,78 @@ void addNode(int value)
     cout << "Node added: " << value << endl;
 }
 
+void addNodeAt(int value, int position)
+{
+    if (position <= 0)
+    {
+        cout << "invalid position, <= 0 given" << endl;
+        return;
+    }
+
+    Node *newNode = new Node();
+    newNode->data = value;
+    newNode->next = nullptr;
+    int count = 1;
+
+    if (head == nullptr)
+    {
+        head = newNode;
+    }
+    else
+    {
+        Node *temp = head, *prev = head;
+
+        while (count++ < position && temp != nullptr)
+        {
+            prev = temp;
+            temp = temp->next;
+        }
+
+        newNode->next = temp;
+
+        if (position == 1)
+            head = newNode;
+        else
+            prev->next = newNode;
+    }
+    cout << "Node added: " << value << endl;
+}
+
+// rewrite of add node at
+void addNodeAt2(int value, int position)
+{
+    if (position <= 0)
+    {
+        cout << "invalid position, <= 0 given" << endl;
+        return;
+    }
+
+    Node *newNode = new Node();
+    newNode->data = value;
+    newNode->next = nullptr;
+    int count = 1;
+
+    if (head == nullptr || position == 1)
+    {
+        newNode->next = head;
+        head = newNode;
+        cout << "Node added: " << value << endl;
+        return;
+    }
+
+    Node *temp = head;
+
+    while (count++ < position - 1 && temp->next != nullptr)
+    {
+        temp = temp->next;
+    }
+
+    newNode->next = temp->next;
+    temp->next = newNode;
+
+    cout << "Node added: " << value << endl;
+}
+
 // Function to delete first occurrence of a value
 void deleteNode(int value)
 {
@@ -89,14 +161,15 @@ void displayList()
 
 int main()
 {
-    int choice, value;
+    int choice, value, position;
     do
     {
         cout << "\n--- Menu ---\n";
         cout << "1. Add Node\n";
         cout << "2. Delete Node\n";
         cout << "3. Display Linked List\n";
-        cout << "4. Exit\n";
+        cout << "4. Add Nod at\n";
+        cout << "5. Exit\n";
         cout << "Enter choice: ";
         cin >> choice;
 
@@ -116,12 +189,19 @@ int main()
             displayList();
             break;
         case 4:
+            cout << "Enter value: ";
+            cin >> value;
+            cout << "Enter position: ";
+            cin >> position;
+            addNodeAt2(value, position);
+            break;
+        case 5:
             cout << "Exiting...\n";
             break;
         default:
             cout << "Invalid choice!\n";
         }
-    } while (choice != 4);
+    } while (choice != 5);
 
     return 0;
 }
